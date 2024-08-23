bc0f39254f7e6e06bc47166026693f97c309cca3
Poyet Nyahuku <129210881+Poyetnyax@users.noreply.github.com>
2024-08-24 00:27:23 +0200

    socials (#5)

--- src/pages/sections/intro/Intro.jsx
+++ src/pages/sections/intro/Intro.jsx
@@ -1,15 +1,29 @@
 import './Intro.css';
 import heroimg from '../../../assets/me2.jpeg';
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 const Intro = () => {
     return (
         <section id="intro" className="intro">
-            <div className="intro__text">
-                <h1>Hello, I'm Poyet 👋</h1>
-                <p>I'm a full stack developer based in Poland 🇵🇱</p>
+            <div className="intro_container">
+                <div className="intro__text">
+                    <h1>Hello, I'm Poyet 👋</h1>
+                    <p>I'm a full stack developer based in Poland 🇵🇱</p>
+                </div>
+                <div className="hero.img">
+                    <img className='img' src={heroimg} alt=""/>
+                </div>
             </div>
-            <div className="hero.img">
-                <img className='img' src={heroimg} alt=""/>
+            <div className="intro_socials">
+                <a href="https://github.com/Poyetnyax" className="contact-link">
+                    <FaGithub />
+                </a>
+                <a href="mailto:poyetnyahuku3@gmail.com" className="contact-link">
+                    <FaEnvelope />
+                </a>
+                <a href="https://www.linkedin.com/in/poyet-nyahuku-365aa4266/" target="_blank" rel="noopener noreferrer" className="contact-link">
+                    <FaLinkedin />
+                </a>
             </div>
         </section>
     );
